4de197ed32de6cfa5c1859960675a6cc8ce147bf
VenkataSN <41758711+VenkataSN@users.noreply.github.com>
2018-08-09 11:37:32 -0500

    Update test10.sql

--- test10.sql
+++ test10.sql
@@ -2,7 +2,7 @@ INSERT INTO edi.code_value
 VALUES      ( code_value_seq.NEXTVAL, 
              (SELECT code_set 
               FROM   edi.code_set 
-              WHERE  code_set_name1 = 'FUNCTIONAL_TRANSACTION_STATUS'), 
+              WHERE  code_set_name = 'FUNCTIONAL_TRANSACTION_STATUS'), 
              'EPAYMENT_RECON_VOID_TRANSACTION_SERVICE_TEST_CHEF', 
              'Service for Epayment Reconciliation Void Transaction for testing chef', 
              1, 
