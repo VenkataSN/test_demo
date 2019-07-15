c4472756e0456f4c0c32eac37eaaa297ff5058b0
VenkataSN <41758711+VenkataSN@users.noreply.github.com>
2019-07-15 14:26:47 -0500

    Update and rename 02_test.sql to 022_test.sql

--- 022_test.sql
+++ 022_test.sql
@@ -4,7 +4,7 @@ VALUES      ( code_value_seq.NEXTVAL,
               FROM   edi.code_set 
               WHERE  code_set_name = 'FUNCTIONAL_TRANSACTION_STATUS'), 
              'EPAYMENT_RECON_JENKINS_SAMPLE', 
-             'Test  For Jenkins Integration', 
+             'Test  For Jenkins Integration_01', 
              1, 
              SYSDATE, 
              SYSDATE, 
