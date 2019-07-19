d9f113ea02ada5c7746c579fa84bdbf67ca1723d
VenkataSN <41758711+VenkataSN@users.noreply.github.com>
2019-07-18 23:58:39 -0500

    Update and rename 011_test.sql to 011_exec_insert.sql

---
+++ 011_exec_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO edi.code_value 
+VALUES      ( code_value_seq.NEXTVAL, 
+             (SELECT code_set 
+              FROM   edi.code_set 
+              WHERE  code_set_name = 'FUNCTIONAL_TRANSACTION_STATUS'), 
+             'EPAYMENT_RECON_JENKINS_SAMPLE', 
+             'Test  For Jenkins Integration_01', 
+             1, 
+             SYSDATE, 
+             SYSDATE, 
+             SYSDATE);
